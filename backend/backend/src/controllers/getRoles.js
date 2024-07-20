508b939751d8a17fbe5ad643a1ac6d854ad1b3ed
Marcao22 <160734930+Marcao22@users.noreply.github.com>
2024-07-19 20:20:24 -0500

    carga de back
    configurar para la coneccion a la bd

---
+++ backend/backend/src/controllers/getRoles.js
@@ -0,0 +1,20 @@
+const { Pool } = require("pg");
+const { CONFIG_BD } = require("../config/db");
+
+const pool = new Pool(CONFIG_BD);
+
+const getRoles = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM roles");
+
+    if (result.rows.length > 0) {
+      return res.status(200).json(result.rows);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  getRoles,
+};
