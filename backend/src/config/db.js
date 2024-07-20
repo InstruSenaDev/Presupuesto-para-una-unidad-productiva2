aa5549895082f281aaebb634a93dfe444eb1b153
Juan Jose Cespedes Villota <120986217+Destroyetpro@users.noreply.github.com>
2024-07-19 21:13:09 -0500

    Configuracion del backend
    Base de datos llamada y prueba del metodo GET trayendo la tabla roles

---
+++ backend/src/config/db.js
@@ -0,0 +1,11 @@
+const CONFIG_BD = {
+  user: "postgres.fpzrttavkfpjrzgjoivb",
+  host: "aws-0-us-west-1.pooler.supabase.com",
+  database: "postgres",
+  password: "?3#Kf2h+$Qf6C8W",
+  port: "6543",
+};
+
+module.exports = {
+  CONFIG_BD,
+};
