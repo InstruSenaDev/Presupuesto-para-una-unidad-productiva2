5033d97e5bba60be3ff1aa153b8de16caaef605a
andresf <andresfelipec574@gmail.com>
2024-07-26 12:04:08 -0500

    arreglos definitivos
    las 3 dash personal, familiar, empresarial,
    dash-inicio

---
+++ frontend/src/components/Paginador.jsx
@@ -0,0 +1,46 @@
+// src/components/PaginatedItems.jsx
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+
+// Ejemplo de elementos para simular la obtención de recursos externos.
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+function Items({ currentItems }) {
+  return (
+    <>
+      {currentItems &&
+        currentItems.map((item) => (
+          <div key={item}>
+            <h3>Item #{item}</h3>
+          </div>
+        ))}
+    </>
+  );
+}
+
+export default function PaginatedItems({ itemsPerPage }) { // Agregar itemsPerPage como prop
+  const [itemOffset, setItemOffset] = useState(0);
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = items.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(items.length / itemsPerPage);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <Items currentItems={currentItems} />
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
+    </>
+  );
+}
