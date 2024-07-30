63d56d2c3f3de7ba3257f931bf0853cab57723ce
Marcao22 <160734930+Marcao22@users.noreply.github.com>
2024-07-29 21:35:07 -0500

    p

--- frontend/src/js/a.js
+++ frontend/src/js/a.js
@@ -1,18 +1,12 @@
-const openModal = document.getElementById('open-modal');
-const closeModal = document.getElementById('close-modal');
-const modal = document.getElementById('modal');
 
-openModal.addEventListener('click', () => {
-  modal.classList.remove('hidden');
-});
+    document.getElementById('open-modal').addEventListener('click', function() {
+    document.getElementById('modal').classList.remove('hidden');
+  });
 
-closeModal.addEventListener('click', () => {
-  modal.classList.add('hidden');
-});
+  document.getElementById('close-modal').addEventListener('click', function() {
+    document.getElementById('modal').classList.add('hidden');
+  });
 
-// Also, you can add an event to close the modal when clicking outside of it
-document.addEventListener('click', (e) => {
-  if (e.target === modal) {
-    modal.classList.add('hidden');
-  }
-});
+  document.getElementById('close-modal-2').addEventListener('click', function() {
+    document.getElementById('modal').classList.add('hidden');
+  });
