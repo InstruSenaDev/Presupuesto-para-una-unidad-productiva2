371d917fb7d1051350bd632242804a9d3c56d921
Marcao22 <160734930+Marcao22@users.noreply.github.com>
2024-07-31 11:51:46 -0500

    ..

--- frontend/src/js/envioForRegistro.js
+++ frontend/src/js/envioForRegistro.js
@@ -48,13 +48,13 @@ form.addEventListener("submit", async (e) => {
 
     if (response.ok) {
       console.log("Registro exitoso");
-      console.log("MAAAAAAAAAAAAAAAAAAAAAMAGUEEEEEEEEEEEEEEEEEEEEEEEEEEEVO 45453")
+      console.log("M45453")
     } else {
       console.error("Error en el registro");
-      console.log("MAAAAAAAAAAAAAAAAAAAAAMAGUEEEEEEEEEEEEEEEEEEEEEEEEEEEVO 1")
+      console.log("M 1")
     }
   } catch (error) {
     console.error("Error al enviar la solicitud:", error);
-    console.log("MAAAAAAAAAAAAAAAAAAAAAMAGUEEEEEEEEEEEEEEEEEEEEEEEEEEEVO 2")
+    console.log("M 2")
   }
 });
