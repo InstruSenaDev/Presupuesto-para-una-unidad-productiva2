00d22cdc93883fb1629d2662ae5a4ed101c546e3
Marcao22 <160734930+Marcao22@users.noreply.github.com>
2024-08-01 21:53:07 -0500

    intento de buscador por la bd

--- frontend/src/js/a.js
+++ frontend/src/js/a.js
@@ -1,12 +1,33 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://fpzrttavkfpjrzgjoivb.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
-    document.getElementById('open-modal').addEventListener('click', function() {
-    document.getElementById('modal').classList.remove('hidden');
-  });
+const form = document.getElementById("Buscador");
 
-  document.getElementById('close-modal').addEventListener('click', function() {
-    document.getElementById('modal').classList.add('hidden');
-  });
+form.addEventListener("submit", async (e) => {
+  e.preventDefault(); // Evita que el formulario se envíe por defecto
+  console.log("evento enviado")
 
-  document.getElementById('close-modal-2').addEventListener('click', function() {
-    document.getElementById('modal').classList.add('hidden');
-  });
+  // Obtener los valores del formulario
+  const buscar = document.getElementById("Buscar").value;
+
+  // Buscar usuarios que coincidan con el nombre o documento
+  try {
+    console.log("preparando solicitud")
+    const { data, error } = await supabase
+      .from('usuarios')
+      .select('*')
+      .filter('nombre', 'like', '%' + buscar + '%')
+      .filter('documento', 'like', '%' + buscar + '%');
+      console.log("busqueda enviada");
+
+    if (error) {
+      console.error("Error al buscar usuarios:", error);
+    } else {
+      console.log(data);
+    }
+  } catch (error) {
+    console.error("Error al enviar la solicitud:", error);
+  }
+});
