6f10fd11b8a7d542872aaa76d7563dbfdabe2180
Marcao22 <160734930+Marcao22@users.noreply.github.com>
2024-08-02 08:37:16 -0500

    .....

--- frontend/src/js/Ma.js
+++ frontend/src/js/Ma.js
@@ -1,26 +1,28 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://fpzrttavkfpjrzgjoivb.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+
+// Asegúrate de que VITE_SUPABASE_KEY esté configurado en tus variables de entorno
+const supabaseUrl = import.meta.env.supabaseUrl;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const form = document.getElementById("Buscador");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault(); // Evita que el formulario se envíe por defecto
-  console.log("evento enviado")
+  console.log("evento enviado");
 
   // Obtener los valores del formulario
   const buscar = document.getElementById("Buscar").value;
 
   // Buscar usuarios que coincidan con el nombre o documento
   try {
-    console.log("preparando solicitud")
+    console.log("preparando solicitud");
     const { data, error } = await supabase
       .from('usuarios')
       .select('*')
-      .filter('nombre', 'like', '%' + buscar + '%')
-      .filter('documento', 'like', '%' + buscar + '%');
-      console.log("busqueda enviada");
+      .or(`nombre.ilike.%${buscar}%, documento.ilike.%${buscar}%`);
+    
+    console.log("busqueda enviada");
 
     if (error) {
       console.error("Error al buscar usuarios:", error);
