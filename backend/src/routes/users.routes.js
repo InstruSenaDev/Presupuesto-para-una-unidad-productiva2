e3de808b98ac7e2199443243dbcff949f61c4646
Juan Jose <120986217+Destroyetpro@users.noreply.github.com>
2024-08-03 06:30:55 -0500

    prueba
    borrando las carpetas para mirar si se borra el otro backend

--- backend/src/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const getRoles = require("../../src/controllers/getRoles");
-
-router.get("/rol", getRoles.getRoles);
-
-module.exports = router;
