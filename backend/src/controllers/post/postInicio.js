a77d480ae02e4d8814d00219fe4d271341158686
Juan Jose <120986217+Destroyetpro@users.noreply.github.com>
2024-08-09 11:57:17 -0500

    localStorage
    faltan las validaciones del localStorage

--- backend/src/controllers/post/postInicio.js
+++ backend/src/controllers/post/postInicio.js
@@ -10,7 +10,7 @@ const iniciarSesion = async (req, res) => {
   try {
     // Buscar un usuario con el correo proporcionado
     const result = await pool.query(
-      "SELECT * FROM usuarios WHERE correo = $1",
+      "SELECT idusuario, correo, contrasena, nombre FROM usuarios WHERE correo = $1",
       [correo]
     );
 
