a990725e2cbc18423ac72ebdf61e7347e9bc8601
Marcos <marcosdavidarroyoescobar@gmail.com>
2024-08-08 21:13:16 -0500

    Nodemail
    enproceso

--- frontend/src/components/p/x.js
+++ frontend/src/components/p/x.js
@@ -1,12 +1,17 @@
-function EnviarCorreo (){
-    let parms ={
+function EnviarCorreo(event) {
+    event.preventDefault();  // Evita el recargo de la página
+    
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subjec").value,
+        message: document.getElementById("message").value,
+    };
 
-name: document.getElementById("name").value,
-email: document.getElementById("email").value,
-subject: document.getElementById("subjec").value,
-message: document.getElementById("message").value,     
-        
-   
-    }
-emailjs.send("service_vxffcka","template_r71f8nl")
-} 
+    emailjs.send("service_fd28oz9", "template_dx4ecid", parms)
+    .then(function(response) {
+        alert("Email enviado con éxito!");
+    }, function(error) {
+        alert("Ocurrió un error al enviar el email: " + JSON.stringify(error));
+    });
+}
