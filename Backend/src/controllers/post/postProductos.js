ef1815782a32bb645cd8c167f2831d4afe2ec355
Carlos Correa <ccorreacervera92@gmail.com>
2024-08-09 18:50:46 -0500

    backend

--- Backend/src/controllers/post/postProductos.js
+++ Backend/src/controllers/post/postProductos.js
@@ -4,13 +4,13 @@
     const pool = new Pool(CONFIG_BD);
 
     const productos = async (req, res) => {
-    const { nombre, descripcion, valorunitario } = req.body;
+    const { nombre, descripcion, codigo, valorunitario } = req.body;
     console.log(nombre);
 
     try {
         const resultMovimiento = await pool.query(
-        "INSERT INTO producto (nombre, descripcion, valorunitario, cantidad) VALUES ($1, $2, $3, $4) RETURNING * ",
-        [nombre, descripcion, valorunitario, "1"]
+        "INSERT INTO producto (nombre, descripcion,codigo, valorunitario, cantidad) VALUES ($1, $2, $3, $4, $5) RETURNING * ",
+        [nombre, descripcion, codigo, valorunitario, "1"]
         );
         res.status(201).json(resultMovimiento.rows[0]);
     } catch (error) {
@@ -18,9 +18,10 @@
         res
         .status(500)
         .json({ message: "Error interno al procesar la solicitud: ", error });
+        console.log(res.status)
     }
     };
 
     module.exports = {
-    productos,
+    productos
     };
--- Backend/src/routes/users.routes.js
+++ Backend/src/routes/users.routes.js
@@ -5,10 +5,12 @@ const {getRoles} = require("../controllers/Get/getRoles");
 const {nuevosUser} = require("../controllers/post/postUsuarios")
 const {productos} = require("../controllers/post/postProductos")
 const {iniciarSesion} = require("../controllers/post/postInicio")
+const {apiProductos} = require("../controllers/Get/getProductos")
 
 router.get("/rol", getRoles);// http://localhost:3000/rol
 router.post("/registro", nuevosUser );// http://localhost:3000/registro
-router.get("/productos", productos);// http://localhost:3000/productos
+router.post("/productos", productos);// http://localhost:3000/productos
 router.post("/inicio", iniciarSesion);// http://localhost:3000/inicio
+router.get("/traerProductos", apiProductos);// http://localhost:3000/api/producto
 
 module.exports = router;
