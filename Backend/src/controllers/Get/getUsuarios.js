812d07b7ebed34779b06bae365655a0bf6d64a3e
Marcos <marcosdavidarroyoescobar@gmail.com>
2024-08-13 07:40:54 -0500

    ..

--- Backend/src/controllers/Get/getUsuarios.js
+++ Backend/src/controllers/Get/getUsuarios.js
@@ -1,22 +1,20 @@
-const { Pool } = require("pg")
-const { CONFIG_BD } = require("../../config/db")
+const {Pool} = require('pg')
+const {CONFIG_BD} = require('../../config/db')
 
-const pool = new Pool(CONFIG_BD);
+const pool = new Pool(CONFIG_BD)
 
-
-
-const getUser = async (req, res) => {
-        try {
-          const result = await pool.query("SELECT * FROM roles");
-      
-          if (result.rows.length > 0) {
-            return res.status(200).json(result.rows);
-          }
-        } catch (error) {
-          console.log(error);
+const getUsuarios = async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM usuario")
+        if (result.rows.length > 0) {
+            return res.status(200).json(result.rows)
         }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message:'error al recuperar usuarios'})
+    }
+}
 
-};
-module.export={
-    getUser,
-};
+module.exports = {
+    getUsuarios
+}
