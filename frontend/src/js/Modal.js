7b413532e3ae6a89f3997bebf24fe4de61197415
CarlosRG2 <160734695+CarlosRG2@users.noreply.github.com>
2024-08-13 09:53:24 -0500

    cambios

---
+++ frontend/src/js/Modal.js
@@ -0,0 +1,31 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://fpzrttavkfpjrzgjoivb.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+document.querySelector('form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+  
+    const fecha = document.getElementById('fecha').value;
+   
+    // Inserta los datos en la tabla "productos"
+    const { data, error } = await supabase
+      .from('presupuesto')
+      .insert([
+        { fecha: fecha }
+      ])
+  
+    if (error) {
+      console.error('Error al insertar datos:', error.message);
+      alert('Hubo un error al guardar el producto.');
+    } else {
+      console.log('Producto guardado:', data);
+      alert('Producto guardado exitosamente!');
+      
+      // Opcional: Cierra el modal y limpia el formulario después de enviar los datos
+      modal.classList.add('hidden');
+      e.target.reset();
+    }
+  });
+  
