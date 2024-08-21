5e55ce6700d484a8473861107d631a1b33a8e719
Andresfelipec20 <160735061+Andresfelipec20@users.noreply.github.com>
2024-08-21 07:11:58 -0500

    Update users.routes.js

--- Backend/src/routes/users.routes.js
+++ Backend/src/routes/users.routes.js
@@ -6,12 +6,11 @@ const {nuevosUser} = require("../controllers/post/postUsuarios")
 const {productos} = require("../controllers/post/postProductos")
 const {iniciarSesion} = require("../controllers/post/postInicio")
 const {apiProductos} = require("../controllers/Get/getProductos")
-const {llamadoUser}= require("../controllers/Get/getUser")
 
 router.get("/rol", getRoles);// http://localhost:3000/rol
 router.post("/registro", nuevosUser );// http://localhost:3000/registro
 router.post("/productos", productos);// http://localhost:3000/productos
 router.post("/inicio", iniciarSesion);// http://localhost:3000/inicio
 router.get("/traerProductos", apiProductos);// http://localhost:3000/api/producto
-router.get("/user",llamadoUser)
+
 module.exports = router;
