3750a6d7811e8e1d5466754993566d3e86faeecc
Juan Jose Cespedes Villota <120986217+Destroyetpro@users.noreply.github.com>
2024-08-22 22:53:06 -0500

    Prueba de producto
    todavia no se reconoce el localStorage para hacer el fetch y saber que productos ha realizado una persona con un id

--- Backend/src/routes/users.routes.js
+++ Backend/src/routes/users.routes.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const {getRoles} = require("../controllers/Get/getRoles");
-const {nuevosUser} = require("../controllers/post/postUsuarios")
-const {productos} = require("../controllers/post/postProductos")
-const {iniciarSesion} = require("../controllers/post/postInicio")
-const {apiProductos} = require("../controllers/Get/getProductos")
-const {postPago} = require("../controllers/post/postPago")
+const { getRoles } = require("../controllers/Get/getRoles");
+const { nuevosUser } = require("../controllers/post/postUsuarios");
+const { productos } = require("../controllers/post/postProductos");
+const { iniciarSesion } = require("../controllers/post/postInicio");
+const { apiProductos } = require("../controllers/Get/getProductos");
+const { postPago } = require("../controllers/post/postPago");
 
-router.get("/rol", getRoles);// http://localhost:3000/rol
-router.post("/registro", nuevosUser );// http://localhost:3000/registro
-router.post("/productos", productos);// http://localhost:3000/productos
-router.post("/inicio", iniciarSesion);// http://localhost:3000/inicio
-router.get("/traerProductos/:idusuario", apiProductos);// `http://localhost:3000/traerProductos/${idusuario}`
-router.post("/postPago", postPago);// http://localhost:3000/postPago
+router.get("/rol", getRoles); // http://localhost:3000/rol
+router.post("/registro", nuevosUser); // http://localhost:3000/registro
+router.post("/productos", productos); // http://localhost:3000/productos
+router.post("/inicio", iniciarSesion); // http://localhost:3000/inicio
+router.get("/traerProductos/:idusuario", apiProductos); // http://localhost:3000/traerProductos/${idusuario}
+router.post("/postPago", postPago); // http://localhost:3000/postPago
+
 module.exports = router;
