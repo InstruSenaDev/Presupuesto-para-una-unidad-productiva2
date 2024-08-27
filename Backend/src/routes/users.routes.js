f33e988d7d8de69766f9b7e8277b6016a6eed17d
Juan Jose <cespedesvillotaj@gmail.com>
2024-08-27 09:01:24 -0500

    ruta

--- Backend/src/routes/users.routes.js
+++ Backend/src/routes/users.routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {getRoles} = require("../controllers/Get/getRoles");
 const {nuevosUser} = require("../controllers/post/postUsuarios")
 const {productos} = require("../controllers/post/postProductos")
-const {iniciarSesion} = require("../controllers/post/postInicio")
+const {iniciarSesion} = require("../controllers/post/postInicio");
+const { apiProductos } = require("../controllers/Get/getProductos");
 
 router.get("/rol", getRoles);// http://localhost:3000/rol
+router.get("/traerProductos/:idusuario", apiProductos);// http://localhost:3000/rol
 router.post("/registro", nuevosUser );// http://localhost:3000/registro
 router.post("/productos", productos);// http://localhost:3000/productos
 router.post("/inicio", iniciarSesion);// http://localhost:3000/inicio
