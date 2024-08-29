1cbd67e0fa99614d1be9dd664525bac4dcff6511
Andresfelipec20 <160735061+Andresfelipec20@users.noreply.github.com>
2024-08-29 07:31:58 -0500

    cambio a php
    .

--- Backend/src/controllers/Get/getUser.js
+++ Backend/src/controllers/Get/getUser.js
@@ -1,20 +1,22 @@
-const { Pool } = require("pg");
+const mysql = require("mysql2/promise");
 const { CONFIG_BD } = require("../../config/db");
 
-const pool = new Pool(CONFIG_BD);
+const pool = mysql.createPool(CONFIG_BD);
 
 const llamadoUser = async (req, res) => {
-  console.log("aaaaaaaaaaaaaaaaaaaaa");
   try {
-    const result = await pool.query(
-      "SELECT nombre,  correo , tipodocumento FROM usuarios"
+    const [rows] = await pool.query(
+      "SELECT nombre, correo, tipodocumento FROM usuarios"
     );
 
-    if (result.rows.length > 0) {
-      return res.status(200).json(result.rows);
+    if (rows.length > 0) {
+      return res.status(200).json(rows);
+    } else {
+      return res.status(404).json({ message: "No se encontraron usuarios" });
     }
   } catch (error) {
     console.log(error);
+    return res.status(500).json({ message: "Error interno del servidor" });
   }
 };
 
