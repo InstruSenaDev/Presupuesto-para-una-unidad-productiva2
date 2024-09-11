7ce49396bfac9485b98c5369b5fb997ec6f5d13b
andres <160735061+Andresfelipec20@users.noreply.github.com>
2024-09-11 09:54:53 -0500

    nuevo
    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import  React  from 'react'
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 // import viteLogo from '/vite.svg'
 import './App.css'
-<<<<<<< HEAD
 import InicioSesion from './components/inicioDeSesion/Inicio';
 import RegistrodeUsuarios from './pages/registro';
 import PagPresupuestos from './pages/PagPresupuestos';
@@ -31,48 +30,4 @@ function App() {
 
 
 
-=======
-// Productos
-import InicioSesion from './components/inicioDeSesion/Inicio';
-// import RegistrodeUsuarios from './pages/registro';
-import Inicio from './pages/Home';
-// import Productos from './components/Empresarial/TablaProductos';
-import Productos from './pages/Productos'
-import Ventas from './pages/ventas';
-import Inisio from './pages/Inisio';
-import RegistroDeUsuarios from './components/RegistroDeUsuarios/Registro';
-import Sidebar from './components/Sidebar/Sidebar';
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/InicioHome';
-import PagPersonal from './components/pag/pagPresupuestoPersonal';
-import PresupuestoPag from './pages/PresupuestoPag';
-
-
-function App() {
-  // const [count, setCount] = useState(0)
-
-  return (
-    <Router>  
-      <Routes>
-        <Route path="/Inisio" element={<Inisio />} /> 
-        <Route path="/Home" element={<Home />} /> 
-        <Route path="/Registro" element={<RegistroDeUsuarios />} />
-        <Route path="/Sidebar" element={<Sidebar />} />
-        <Route path="/Navbar" element={<Navbar />} />
-        <Route path="/PagPersonal" element={<PagPersonal />} />
-        <Route path="/Presupuestos" element={<PresupuestoPag />} />
-       
-
-
-        <Route path='/' element = {<InicioSesion/>} />
-        <Route path="/Inicio" element={<Inicio />} />
-        {/* <Route path='/RegistrodeUsuario' element = {<RegistrodeUsuarios/>}/> */}
-        <Route path='/Productos' element = {<Productos/>}/>
-        <Route path='/ventas' element = {<Ventas/>}/>
-      </Routes>
-    </Router>
-  )
-}
-
->>>>>>> Juan
 export default App
--- frontend/src/pages/registro.jsx
+++ frontend/src/pages/registro.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import Registro from '../components/registroDeUsuarios/Registro'
-=======
-import Registro from '../components/RegistroDeUsuarios/Registro'
->>>>>>> Juan
 
 
 const RegistrodeUsuarios =  () =>{
